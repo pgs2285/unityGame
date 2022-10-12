1fc6a22f454f15ef66224a4d2825955b66468c10
pgs2285 <wldnjs2284@naver.com>
2022-10-12 15:18:45 +0900

    2020-10-12 / Ground Prefab 기존 prefab들과 연동 안되던 문제 해결

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public GameObject mainCharacter;
+    public GameObject ourObject;
 
     // Update is called once per frame
     void Start()
     {
         Instantiate(mainCharacter, new Vector3(0,0,-1), Quaternion.identity); // MainCharacter 생성하기 매개변수(생성객체, 위치, 회전값);
         // 즉 0,0,-1 에 기본 회전값을 가지고 생성된다
+    }
+
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit,2f))
+        {
+            ourObject=hit.collider.gameObject;
+            Debug.Log ("object that was hit: "+ourObject);
+        }
+    
     }
 }
