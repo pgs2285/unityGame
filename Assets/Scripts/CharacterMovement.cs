04f8e2849366a56b2c955d23ac03a61f74423437
pgs2285 <wldnjs2284@naver.com>
2022-10-14 16:15:29 +0900

    2022-10-14 / 이동함수 대거 수정 /

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,147 +7,145 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public GameObject mainCharacter; // 플레이어 게임 오브젝트
-    public GameObject playerTile; // 플레이어의 시작 타일
-    private GameObject clickedTile;
-    public bool isMoving = false;
-    //public GameObject moveableTile;
-    string groundAxis; //이동가능 위치를 표기해주기 위한 변수
-    private int player_x, player_y, clicked_x, clicked_y;
-    Vector3 destination;
-    public int range = 1; // 이동가능 길이
-    bool ismove = true;
     
-
+    public int range = 1; // 이동가능 길이
+    bool clickable = false;
+    public float speed = 10.0f;
     // Update is called once per frame
+    public GameObject MainCharacterLocated; // 캐릭터의 시작위치 등록
+    public GameObject MoveableMarker;
+    Vector3 destination;
+
+    int state  = 1;
     void Start()
     {
+        destination = MainCharacterLocated.transform.position;
+        // checkMoveable(range);
         // Instantiate(mainCharacter, new Vector3(0,0,-1), Quaternion.identity); // MainCharacter 생성하기 매개변수(생성객체, 위치, 회전값);
-        // 즉 0,0,-1 에 기본 회전값을 가지고 생성된다
+    
     }
 
     void Update()
-    {
-        if (ismove)
-        {
-            string[] previousAxis = playerTile.name.Split(",");
-            player_x = Convert.ToInt32(previousAxis[0]);
-            player_y = Convert.ToInt32(previousAxis[1]);
-            showRoute(player_x, player_y, range);
-            ismove = false;
+    {   
+
+        switch(state){
+            case 1: // 평범한 이동 range값 조정으로 상하좌우 범위 증가 가능
+            basicMove();
+            break;
         }
-        
-        if (Input.GetMouseButtonDown(0)) { // 마우스가 클릭되면 0,1,2 왼쪽,오른쪽,휠
-            basicMovement(player_x, player_y, range); // 어떠한 효과도 받지않은 기본 움직임 (상하좌우만 구현)  
-            ismove = true;
-        }  
+
+    }
     
+    void basicMove(){
+        if (Input.GetMouseButtonDown(0) && clickable){   
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // 카메라로 부터 마우스 위치에 Ray를 쏜다
+
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);        //발사위치, 발사 방향, 발사거리
+
+
+            //가상의 “레이저 빔”을 원점에서부터 레이에 따라 씬 안의 콜라이더에 충돌할 때까지 보냄,  RaycastHit 오브젝트의 충돌된 점에 대한 정보를 반환합니다. 
+            if(isMoveable(hit, range)){ // 범위를 넘어서 클릭하진 않았나 체크
+                destination = hit.collider.gameObject.transform.position; // 사용자가 클릭한 타일
+
+                MainCharacterLocated = hit.collider.gameObject;
+
+                clickable = false; // 이동중에 다시 클릭할 경우 움직이지 않게 하기위함
+            }
+
+        }
+
+        if(transform.position != destination){ //완전히 이동될때까지
+        DestroyMoveableTile();
+        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+
+        }else {
+            if(!clickable) checkMoveable(range); // 한번만 찍게 메모리 낭비 X
+            clickable = true; // 이동완료하면 다음으로 이동가능하게
+
+        }
     }
 
-    void basicMovement(int prev_x, int prev_y,int range){ // range 는 상하좌우 각각 이동가능 길이 
+    GameObject temp;
+    void checkMoveable(int range){ // 사용자가 현재 이동가능한 위치를 클릭하였는가?
 
-        string[] previousAxis = playerTile.name.Split(",");// 이전의 캐릭터 위치 좌표값을 저장하는 previousAxis[0], previousAxis[1]
+       
+        string[] nowLocation = MainCharacterLocated.name.Split(",");
+        int x = Convert.ToInt32(nowLocation[0]);
+        int y = Convert.ToInt32(nowLocation[1]);
+        for(int i = 0; i < range; i++){
+        
+            try{
+
+                temp = GameObject.Find("/9by9_Ground/" + (x + i + 1) + "," + (y)); // range만큼 이동가능한 범위 지정
+                Instantiate(MoveableMarker, temp.transform.position, Quaternion.identity);
+
+            }catch(Exception e){
+                Debug.Log(e.ToString());
+            }
+
+            try{
+
+                temp = GameObject.Find("/9by9_Ground/" + (x - i - 1) + "," + (y)); // range만큼 이동가능한 범위 지정
+                Instantiate(MoveableMarker, temp.transform.position, Quaternion.identity);
+
+            }catch(Exception e){
+                Debug.Log(e.ToString());
+            }
+
+            try{
+
+                temp = GameObject.Find("/9by9_Ground/" + (x) + "," + (y + i + 1)); // range만큼 이동가능한 범위 지정
+                Instantiate(MoveableMarker, temp.transform.position, Quaternion.identity);
+                
+
+            }catch(Exception e){
+                Debug.Log(e.ToString());
+            }
+
+            try{
+
+                temp = GameObject.Find("/9by9_Ground/" + (x) + "," + (y - i - 1)); // range만큼 이동가능한 범위 지정
+                Instantiate(MoveableMarker, temp.transform.position, Quaternion.identity);
+
+            }catch(Exception e){
+                Debug.Log(e.ToString());
+            }
 
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // 카메라로 부터 마우스 위치에 Ray를 쏜다
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);        //발사위치, 발사 방향, 발사거리
-        //가상의 “레이저 빔”을 원점에서부터 레이에 따라 씬 안의 콜라이더에 충돌할 때까지 보냄,  RaycastHit 오브젝트의 충돌된 점에 대한 정보를 반환합니다. 
-
-        clickedTile = hit.collider.gameObject; // 사용자가 클릭한 타일
-        string[] clickedAxis = clickedTile.name.Split(","); /// 사용자가 클릭한 파일 좌표 split
-
-
-        try // 연산을 위해 - 작업
-        {
-            prev_x = Convert.ToInt32(previousAxis[0]);
-            prev_y = Convert.ToInt32(previousAxis[1]);
-            clicked_x = Convert.ToInt32(clickedAxis[0]);
-            clicked_y = Convert.ToInt32(clickedAxis[1]);
-        }catch (Exception e){
-           Debug.LogError(e.ToString());
         }
 
-        if (((prev_x - clicked_x == range)|| (prev_x - clicked_x == -range)) && (prev_y == clicked_y) ) // 플레이어 기준 왼쪽 혹은 오른쪽 클릭했을시
-        {
-            destination = hit.collider.gameObject.transform.position;            // 클릭한 좌표를 목적지로
-            destination.z = -1;            // Character가 앞에 있어야하므로 z 위치는 -1 
-            mainCharacter.transform.position = destination;//캐릭터 목적지로 이동
-            playerTile = clickedTile;
-        }
-        else if(((prev_y-clicked_y == range)|| prev_y - clicked_y == - range) && (prev_x == clicked_x))// 플레이어 기준 위 혹은 아래 클릭했을시
-        {
-            destination = hit.collider.gameObject.transform.position;            // 클릭한 좌표를 목적지로
-            destination.z = -1;            // Character가 앞에 있어야하므로 z 위치는 -1 
-            mainCharacter.transform.position = destination;//캐릭터 목적지로 이동
-            playerTile = clickedTile;
-        }
-        else
-        {
-            Debug.Log("이동할 수 없는 좌표입니다");
-        }
 
     }
-
-    public GameObject moveableMarker;
-    private GameObject temp;
-    Vector3 markerLocate;
-    GameObject[] trash; 
-    List<GameObject> markerList = new List<GameObject>();
-    public void showRoute(int x, int y, int range)
-    {
+    GameObject[] trash;
+    void DestroyMoveableTile(){
         trash = GameObject.FindGameObjectsWithTag("marker");
         foreach(GameObject trash in trash)
         {
             Destroy(trash);
         }
-        // Instantiate(mainCharacter, new Vector3(0,0,-1), Quaternion.identity); // MainCharacter 생성하기 매개변수(생성객체, 위치, 회전값);
-        try
-        {  
-            temp = GameObject.Find("/9by9_Ground/" + (x + range) + "," + (y));
-            markerLocate = temp.transform.position;
-            markerLocate.z = -0.5f;
-            Instantiate(moveableMarker, markerLocate, Quaternion.identity);
-
-        }
-        catch (Exception e)
-        {
-            Debug.Log(e.ToString());
-        }
-
-        try
-        {
-            temp = GameObject.Find("/9by9_Ground/" + (x - range) + "," + (y));
-            markerLocate = temp.transform.position;
-            markerLocate.z = -0.5f;
-            Instantiate(moveableMarker, markerLocate, Quaternion.identity);
-
-        }catch(Exception e)
-        {
-            Debug.Log(e.ToString());
-        }
-
-        try
-        {
-            temp = GameObject.Find("/9by9_Ground/" + (x) + "," + (y + range));
-            markerLocate = temp.transform.position;
-            markerLocate.z = -0.5f;
-            Instantiate(moveableMarker, markerLocate, Quaternion.identity);
-
-        }catch(Exception e)
-        {
-            Debug.Log(e.ToString());
-        }
-
-        try
-        {
-            temp = GameObject.Find("/9by9_Ground/" + (x) + "," + (y - range));
-            markerLocate = temp.transform.position;
-            markerLocate.z = -0.5f;
-            Instantiate(moveableMarker, markerLocate, Quaternion.identity);
-        }catch(Exception e)
-        {
-            Debug.Log(e.ToString());
-        }
     }
 
+    bool isMoveable(RaycastHit2D hit,int range){ // 이동가능한 좌표를 클릭했는가 체크
+        int prev_x, prev_y, clicked_x, clicked_y;
+
+        try{
+            string[] prevAxis = MainCharacterLocated.name.Split(",");
+            string[] clickedAxis = hit.collider.gameObject.name.Split(",");
+            
+            clicked_x = Convert.ToInt32(clickedAxis[0]);
+            clicked_y = Convert.ToInt32(clickedAxis[1]);
+            prev_x = Convert.ToInt32(prevAxis[0]);
+            prev_y = Convert.ToInt32(prevAxis[1]);
+
+        }catch(Exception e){
+            Debug.Log(e.ToString());
+            return false;
+        }
+        for(int i = 1; i <= range; i++){
+            if (((prev_x - clicked_x == i)|| (prev_x - clicked_x == -i)) && (prev_y == clicked_y)) return true;
+            else if(((prev_y-clicked_y == i)|| prev_y - clicked_y == - i) && (prev_x == clicked_x)) return true; 
+        }
+        return false;
+    }
 }
