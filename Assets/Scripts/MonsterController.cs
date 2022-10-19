8523acd6cd4d1b23b777c629c9c39f1691a5cc15
pgs2285 <wldnjs2284@naver.com>
2022-10-19 16:22:48 +0900

    2022-10-19 / 턴 시스템 구현, 돼지 움직임 구현했으나 if조건 보충, 마우스 올라가있으면 mob 정보 표기

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -2,25 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MonsterController : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public TextMeshProUGUI cost;
     public GameObject[] Monster;
+    // public GameObject PigIndicator;
+    public Image monsterImage; //UI Canvas 에 띄울 image 변수
+    public Image monsterMoveIndicator;
+    private Sprite blankImage;
+    public GameObject player;
 
-    
+    public bool playerTurn;
+
     void Start()
     {   
         for(int i = 0; i < Monster.Length; i++)
         {
           Instantiate(Monster[i]);
+          
+          blankImage = monsterImage.sprite;
         }
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   checkTurnEnd();
+        IndicateImage();
     }
+
+    void IndicateImage(){
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // 카메라로 부터 마우스 위치에 Ray를 쏜다
+      RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);  //발사위치, 발사 방향, 발사거리
+      if(hit.collider.gameObject.tag == "enemy"){
+        
+        monsterImage.sprite = hit.collider.gameObject.GetComponent<SpriteRenderer>().sprite; // 돼지 UI에 띄우기
+        monsterMoveIndicator.sprite = hit.collider.gameObject.GetComponent<crossMovement>().moveIndicator;
+      }else{
+        monsterImage.sprite = blankImage;
+        monsterMoveIndicator.sprite = blankImage;
+      }
+    }
+    public int checkTurn = 0;
+
+
+    void checkTurnEnd(){
+      for(int i = 0; i< Monster.Length; i++){
+
+        if(checkTurn >= Monster.Length && playerTurn == false){ //만약 monster들이 전부다 turn을 마쳤고, 내 턴이 종료된 상태라면
+          playerTurn = true;
+          cost.text = "5";
+          checkTurn = 0;
+        }
+      }
+    }
+
+
+    public void PlayerTurnSet()
+    {
+      playerTurn = false;
+    }
+
+
+
 }
---
+++ Assets/Scripts/pig/crossMovement.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class crossMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject Ground;
+    private Vector3 moveToScreen;
+    private float range = 1;
+    private int speed = 10;
+    public bool turnEnd;
+    Vector3 checkGround;
+    public Sprite moveIndicator;
+
+    private const int LEFT = 1;
+    private const int RIGHT = 2;
+    private const int DOWN = 3;
+    private const int UP = 4;
+    private const int ERROR = 0;
+
+    private GameObject system;
+
+    void Start()
+    {
+        Ground = GameObject.FindGameObjectWithTag("groundPrefs");
+        range = range * Ground.transform.localScale.x;
+        moveToScreen.x = range * Random.Range(-4, 4) + Ground.transform.position.x;
+        moveToScreen.y = range * Random.Range(-4, 4) + Ground.transform.position.y;
+        system = GameObject.FindGameObjectWithTag("system");
+        
+    }
+
+    void FixedUpdate()
+    {   
+        if(!(system.GetComponent<MonsterController>().playerTurn)){
+            Debug.Log("이동모드");
+            pigMovePattern();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, moveToScreen, Time.deltaTime * speed);
+    }
+
+    void pigMovePattern(){
+        while(true){
+            int direction = Random.Range(1,5); // 1~4 까지 이동
+            Debug.Log(direction);
+            checkGround = moveToScreen;
+            if(direction == UP){
+                Debug.Log("move UP");
+                checkGround.y += range;
+                moveToScreen = checkGround;
+                system.GetComponent<MonsterController>().checkTurn += 1;
+                
+            }else if(direction == DOWN){
+                Debug.Log("move DOWN");
+                checkGround.y -= range;
+                moveToScreen = checkGround;
+                system.GetComponent<MonsterController>().checkTurn += 1;
+                
+            }else if(direction == LEFT){
+                Debug.Log("move LEFT");
+                checkGround.x -= range;
+                moveToScreen = checkGround;
+                system.GetComponent<MonsterController>().checkTurn += 1;
+                
+            }else if(direction == RIGHT){
+                Debug.Log("move RIGHT");
+                checkGround.x += range;
+                moveToScreen = checkGround;
+                system.GetComponent<MonsterController>().checkTurn += 1;
+                
+            }
+            break;
+        }
+    }
+
+
+
+}
